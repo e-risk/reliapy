23d5642ee6a42bf5d293ee447f0132003cf0a2ce
ketsonroberto <ketson.santos@gmail.com>
2020-06-19 23:09:14 -0400

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reliapy',
-    version="0.1.0",
+    version='0.1',
     url='https://github.com/e-risk/reliapy',
     description="UQpy is a general purpose toolbox for Uncertainty Quantification",
     author="Ketson R. M. dos Santos",

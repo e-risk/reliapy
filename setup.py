6a23e69719d63dc9a2f739b5ebee0910d21b4abf
Ketson R. M. dos Santos <47835396+ketsonroberto@users.noreply.github.com>
2020-06-19 21:00:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   author = 'Ketson R. M. dos Santos',                   # Type in your name
   author_email = 'reliapy.py@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/e-risk/reliapy/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Structural Reliability', 'Stochastic Process', 'Probability'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
